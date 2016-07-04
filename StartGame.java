3b56126091b0c54d6b5e453fe25a6ecc44c0554a
Archelite <archelite011@gmail.com>
2016-07-04 22:31:18 +0300

    Update and rename LICENSE.md to StartGame.java

---
+++ StartGame.java
@@ -0,0 +1,11 @@
+package minesweeper;
+
+public class StartGame {
+
+    public static Interface INTERFACE;
+    
+    public static void main(String[] args) {
+        INTERFACE = new Interface();
+    }
+    
+}
